16c78c4cb8c487d5144b4753713c532b45641a0b
Dejan Samardzija <samardzija.dejan@gmail.com>
2014-08-26 22:33:33 +0200

    updated parameters

--- src/Parameters.php
+++ src/Parameters.php
@@ -4,26 +4,34 @@ namespace G4\Constants;
 
 class Parameters
 {
-    const ID            = 'id';
-    const PAGE          = 'page';
-    const PER_PAGE      = 'per_page';
-    const COLLECTION    = 'collection';
-    const RESOURCE      = 'resource';
-    const USERNAME      = 'username';
-    const USER_ID       = 'user_id';
-    const EMAIL         = 'email';
-    const SENDER        = 'sender';
-    const RECEIVER      = 'receiver';
-    const CC            = 'cc';
-    const BCC           = 'bcc';
-    const SUBJECT       = 'subject';
-    const BODY          = 'body';
-    const DATA          = 'data';
-    const IP_ADDRESS    = 'ip';
-    const NAME          = 'name';
-    const STATUS        = 'status';
-    const TYPE          = 'type';
-    const REQUIRED      = 'required';
-    const DEFAULT_VALUE = 'default';
-    const SESSION       = 'session';
+    const ID              = 'id';
+    const PAGE            = 'page';
+    const PER_PAGE        = 'per_page';
+    const COLLECTION      = 'collection';
+    const RESOURCE        = 'resource';
+    const USERNAME        = 'username';
+    const USER_ID         = 'user_id';
+    const EMAIL           = 'email';
+    const SENDER          = 'sender';
+    const RECEIVER        = 'receiver';
+    const CC              = 'cc';
+    const BCC             = 'bcc';
+    const SUBJECT         = 'subject';
+    const BODY            = 'body';
+    const DATA            = 'data';
+    const IP_ADDRESS      = 'ip';
+    const NAME            = 'name';
+    const STATUS          = 'status';
+    const TYPE            = 'type';
+    const REQUIRED        = 'required';
+    const DEFAULT_VALUE   = 'default';
+    const SESSION         = 'session';
+
+    const CC_NUMBER       = 'card_number';
+    const CC_NAME         = 'card_name';
+    const CC_CVV2         = 'card_cvv2';
+    const CC_EXPIRE_MONTH = 'card_expire_month';
+    const CC_EXPIRE_YEAR  = 'card_expire_year';
+
+    const AUTH_KEY_NAME   = 'X-ND-Authentication';
 }
