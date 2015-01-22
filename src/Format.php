a3ec1d5af3b47aa6fd5a4f193901e8d16198b5f7
Drasko Gomboc <drasko.gomboc@gmail.com>
2015-01-22 09:47:05 +0100

    Added Format constants

---
+++ src/Format.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace G4\Constants;
+
+class Format
+{
+    const FORMAT = 'format';
+
+    const HTML = 'html';
+    const JSON = 'json';
+    const TWIG = 'twig';
+    const XML  = 'xml';
+}
