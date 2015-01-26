9bda894a3d387e79cae04760bca2aeeb64350ccb
Drasko Gomboc <drasko.gomboc@gmail.com>
2015-01-26 16:19:19 +0100

    Added Env consts

---
+++ src/Env.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace G4\Constants;
+
+class Env
+{
+    const PRODUCTION = 'production';
+    const STAGE      = 'stage';
+    const DEV        = 'dev';
+    const LOCAL      = 'local';
+}
