3bc251d3f5e9978c8a7d29aaf6d8e6466f941a7d
Drasko Gomboc <drasko.gomboc@gmail.com>
2015-06-08 16:31:47 +0200

    Added Beta as Env; New consts for ContentType

---
+++ src/ContentType.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace G4\Constants;
+
+class ContentType
+{
+    const HTML = 'text/html; charset=UTF-8';
+    const JSON = 'application/json; charset=UTF-8';
+}
--- src/Env.php
+++ src/Env.php
@@ -5,6 +5,7 @@ namespace G4\Constants;
 class Env
 {
     const PRODUCTION = 'production';
+    const BETA       = 'beta';
     const STAGE      = 'stage';
     const DEV        = 'dev';
     const LOCAL      = 'local';
