f0b6acb59f5b817ebec838ec3e360b28b009a8a5
Drasko Gomboc <drasko.gomboc@gmail.com>
2015-06-17 13:38:35 +0200

    Added new params constants

--- src/Parameters.php
+++ src/Parameters.php
@@ -4,36 +4,54 @@ namespace G4\Constants;
 
 class Parameters
 {
-    const ID              = 'id';
-    const PAGE            = 'page';
-    const PER_PAGE        = 'per_page';
-    const COLLECTION      = 'collection';
-    const RESOURCE        = 'resource';
-    const USERNAME        = 'username';
-    const USER_ID         = 'user_id';
-    const EMAIL           = 'email';
-    const SENDER          = 'sender';
-    const RECEIVER        = 'receiver';
-    const CC              = 'cc';
-    const BCC             = 'bcc';
-    const SUBJECT         = 'subject';
-    const BODY            = 'body';
-    const DATA            = 'data';
-    const IP_ADDRESS      = 'ip';
-    const NAME            = 'name';
-    const STATUS          = 'status';
-    const TYPE            = 'type';
-    const REQUIRED        = 'required';
-    const DEFAULT_VALUE   = 'default';
-    const SESSION         = 'session';
+    const ID               = 'id';
+    const PAGE             = 'page';
+    const PER_PAGE         = 'per_page';
+    const LIMIT            = 'limit';
+    const COLLECTION       = 'collection';
+    const RESOURCE         = 'resource';
 
-    const CC_NUMBER       = 'card_number';
-    const CC_NAME         = 'card_name';
-    const CC_CVV2         = 'card_cvv2';
-    const CC_EXPIRE_MONTH = 'card_expire_month';
-    const CC_EXPIRE_YEAR  = 'card_expire_year';
+    const USERNAME         = 'username';
+    const USER_ID          = 'user_id';
+    const EMAIL            = 'email';
+    const PASSWORD         = 'password';
 
-    const X_ND_APP_KEY    = 'X-ND-AppKey';
-    const X_ND_APP_TOKEN  = 'X-ND-AppToken';
-    const X_ND_AUTH       = 'X-ND-Authentication';
+    const GENDER           = 'gender';
+    const BIRTHDAY         = 'birthday';
+    const BIRTHDAY_DAY     = 'birthday_day';
+    const BIRTHDAY_MONTH   = 'birthday_month';
+    const BIRTHDAY_YEAR    = 'birthday_year';
+
+    const CITY_ID          = 'city_id';
+    const STATE_ID         = 'state_id';
+    const COUNTRY_ID       = 'country_id';
+    const COUNTRY_CODE     = 'country_code';
+
+    const SENDER           = 'sender';
+    const RECEIVER         = 'receiver';
+    const CC               = 'cc';
+    const BCC              = 'bcc';
+    const SUBJECT          = 'subject';
+    const BODY             = 'body';
+    const DATA             = 'data';
+
+    const IP_ADDRESS       = 'ip';
+    const NAME             = 'name';
+    const STATUS           = 'status';
+    const TYPE             = 'type';
+    const REQUIRED         = 'required';
+    const DEFAULT_VALUE    = 'default';
+    const SESSION          = 'session';
+    const AGREE_WITH_TERMS = 'agree_with_terms';
+    const TERM             = 'term';
+
+    const CC_NUMBER        = 'card_number';
+    const CC_NAME          = 'card_name';
+    const CC_CVV2          = 'card_cvv2';
+    const CC_EXPIRE_MONTH  = 'card_expire_month';
+    const CC_EXPIRE_YEAR   = 'card_expire_year';
+
+    const X_ND_APP_KEY     = 'X-ND-AppKey';
+    const X_ND_APP_TOKEN   = 'X-ND-AppToken';
+    const X_ND_AUTH        = 'X-ND-Authentication';
 }
