0da32653e47b3e2a51b7bbe4c07eab9dfec1fc48
Ivan Krickovic <ivankoni@gmail.com>
2015-10-15 14:14:21 +0200

    new Affiliate params: ct and bb_track_code and new general param "wl"

--- src/Affiliate.php
+++ src/Affiliate.php
@@ -5,12 +5,14 @@ namespace G4\Constants;
 class Affiliate
 {
 
-    const AFF_B  = 'aff_b';
-    const AFF_C  = 'aff_c';
-    const AFF_R  = 'aff_r';
-    const AFF_T  = 'aff_t';
-    const AFF_W  = 'aff_w';
-    const MOBILE = 'mobile';
-    const HASH   = 'hash';
-    const FK_ID  = 'fk_id';
+    const AFF_B         = 'aff_b';
+    const AFF_C         = 'aff_c';
+    const AFF_R         = 'aff_r';
+    const AFF_T         = 'aff_t';
+    const AFF_W         = 'aff_w';
+    const MOBILE        = 'mobile';
+    const HASH          = 'hash';
+    const FK_ID         = 'fk_id';
+    const CT            = 'ct';
+    const BB_TRACK_CODE = 'bb_track_code';
 }
